2f7223e8a4eca0ffeb82f8c397ea8d0519252610
Karan01ingh <Singhkunal05046@gmail.com>
2024-01-30 23:04:53 +0530

    completed my first project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
-
 function App(){
-  return (
+  let [color,setColor]=useState("black");
+  return(
     <>
-    <h1>teri maa ka bhosda</h1>
+    <div style={{backgroundColor:color,height:800,width:1500}}>
+    <button onClick={()=>setColor("red")}>red</button>
+    <button onClick={()=>setColor("black")}>black</button>
+    <button onClick={()=>setColor("pink")}>pink</button>
+    <button onClick={()=>setColor("orange")}>orange</button>
+    <button onClick={()=>setColor("white")}>white</button>
+    <button onClick={()=>setColor("purple")}>purple</button> 
+    <button onClick={()=>setColor("olive")}>olive</button> 
+    <button onClick={()=>setColor("green")}>green</button> 
+    <button onClick={()=>setColor("grey")}>grey</button> 
+    <button onClick={()=>setColor("yellow")}>yellow</button> 
+    </div>
     </>
   )
 }
